cd757ac31aff32d9c6a94ae971cba62cc5cb9a40
mrlmaia <murilomaia.bb@gmail.com>
2020-02-09 19:28:10 -0300

    Buscando dados em api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import api from './services/api'
     
 import './styles.css'
 
+import Main from "./pages/main";
 import Header from './components/Header'
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 )
 
